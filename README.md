d741b4614d8893b265608ad81b9fc0fca1f7bc75
gusthls <gustavohls.work@hotmail.com>
2024-07-20 20:05:18 -0300

    Adição da função alternar_estado e da @property ativo

--- README.md
+++ README.md
@@ -8,3 +8,10 @@
   - Demonstrei a utilização do construtor __init__, o qual é responsável por inicializar os atributos da instância, incluindo a definição padrão de ativo como False.
   - Explorei a criação de instâncias da classe Restaurante, destacando como atribuir valores aos atributos da instância durante a criação de um objeto.
   - Compreendi a distinção entre atributos de classe e atributos de instância, e foquei como os atributos específicos da instância são acessados e manipulados dentro da classe.
+
+
+- Aula 3
+  - Explorei como criar atributos em classes e utilizando 'underscore' (underline) para indicar que um atributo é protegido.
+  - Pratiquei o uso da função property em outros atributos, como categoria e ativo, proporcionando uma abordagem mais controlada e facilitando o acesso aos valores desses atributos.
+  - Criei e utilizei métodos de classe, que são métodos que agem sobre a classe como um todo, em vez de uma instância específica. No exemplo, criei o método listar_restaurantes para exibir uma lista formatada dos restaurantes.
+  - Demonstrei o conceito de abstração ao utilizar a propriedade ativo para apresentar visualmente o estado ativo ou inativo de um restaurante.
--- models/restaurante.py
+++ models/restaurante.py
@@ -2,21 +2,31 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
+        self._ativo = False
+
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f"{self.nome} | {self.categoria}"
+        return f"{self._nome} | {self._categoria}"
 
-    @staticmethod
-    def listar_restaurante():
-        for restaurante in Restaurante.restaurantes:
-            print(f"{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}")
+    @classmethod
+    def listar_restaurante(cls):
+        print(f"{'Nome do restaurante'.ljust(20)} | {'Categoria'.ljust(20)} | {'Status'}")
 
+        for restaurante in cls.restaurantes:
+            print(f"{restaurante._nome.ljust(20)} | {restaurante._categoria.ljust(20)} | {restaurante.ativo}")
 
-restaurante_praca = Restaurante('Praça', 'Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+    @property
+    def ativo(self):
+        return '✔' if self._ativo else '✘'
+
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 Restaurante.listar_restaurante()
